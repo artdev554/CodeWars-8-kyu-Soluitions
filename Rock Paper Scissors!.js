58a68c773a2911fb47fc056ae1549cc6b7b2a736
artdev554 <156089415+artdev554@users.noreply.github.com>
2024-01-24 20:20:53 +0200

    Create Rock Paper Scissors!.js

---
+++ Rock Paper Scissors!.js
@@ -0,0 +1,15 @@
+/*
+Description:
+Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+
+*/
+
+
+const rps = (p1, p2) => p1 === p2 ? "Draw!" : (p1 === "rock" && p2 === "scissors") || (p1 === "paper" && p2 === "rock") || (p1 === "scissors" && p2 === "paper")? "Player 1 won!" : "Player 2 won!";
